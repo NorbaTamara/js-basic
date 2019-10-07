0a9d1e2702f429df1641456de199b94564b197e9
Tamara Norba <tno@ciklum.com>
2019-10-07 19:07:17 +0300

    homework for variable-declarations

---
+++ Exercises/variables-declaration/variables-declaration.js
@@ -0,0 +1,15 @@
+// Declare a variable which will be encapsulated from global scope.
+{
+    let incapsulatedVarible;
+}
+
+// Declare a variable which will appear in global object.
+var globalVariable;
+
+// Declare two variables which will be same-names but contain different values.
+var sameName = 5;
+var sameName = 10;
+
+// Declare a variable with hosting.
+hoistedVariable = 'Hi';
+var hoistedVariable;
