1e5ac5cd817a5880514656cfb4c923a86401a8ab
TamaraNorba <norba.tamara@gmail.com>
2019-10-19 16:58:55 +0300

    Update data-types-overview.js

--- Exercises/data-types-overview/data-types-overview.js
+++ Exercises/data-types-overview/data-types-overview.js
@@ -2,20 +2,20 @@
 // 1. Declare a variable on each type using two options: plain creation, creation with constructor.
 // 2. Use operator typeof on each of them.
 //number
-let plainNum = 10;
-typeof plainNum;
-let constructorNum = new Number(100);
-typeof constructorNum;
+let plainNumber = 10;
+typeof plainNumber;
+let constructorNumber = new Number(100);
+typeof constructorNumber;
 //string
-let plainStr = 'hi';
-typeof plainStr;
-let constructorStr = new String ('hello');
-typeof constructorStr;
+let plainString = 'hi';
+typeof plainString;
+let constructorString = new String('hello');
+typeof constructorString;
 //boolean
-let plainBool = true;
-typeof plainBool;
-let constructorBool = new Boolean(false);
-typeof constructorBool;
+let plainBoolean = true;
+typeof plainBoolean;
+let constructorBoolean = new Boolean(false);
+typeof constructorBoolean;
 
 //null
 let variableNull = null;
@@ -25,32 +25,32 @@ let variableUndefined = undefined;
 typeof variableUndefined;
 
 //array
-let plainArr = [1, 2, 3];
-typeof plainArr;
-let constructorArr = new Array('a', 'b', 'c');
-typeof constructorArr;
+let plainArray = [1, 2, 3];
+typeof plainArray;
+let constructorArray = new Array('a', 'b', 'c');
+typeof constructorArray;
 //object
-let plainObj = {a: 1, b: 2, c: 3};
-typeof plainObj;
+let plainObject = {a: 1, b: 2, c: 3};
+typeof plainObject;
 let constructorObj = new Object({a: 'a', b: 'b', c: 'c'});
-typeof constructorObj;
+typeof constructorObject;
 
 // 3. Demonstrate object's references.
-let originObj = {
+let originObject = {
     title: 'course',
     content: 'js',
 };
 
-let copiedObj = originObj; // objects are referenced to the same place in memory so changing of one will cause changing of other
+let copiedObject = originObject; // objects are referenced to the same place in memory so changing of one will cause changing of other
 
-copiedObj.level = 'basic';
-console.log(originObj);
-console.log(copiedObj); // the same content with originObj
+copiedObject.level = 'basic';
+console.log(originObject);
+console.log(copiedObject); // the same content with originObj
 
-let properlyCopiedObj = {...originObj}; //example how we can avoid duplicating
-properlyCopiedObj.level = 'basic level';
-console.log(originObj);
-console.log(properlyCopiedObj); // properlyCopiedObject can be changed independently from origin
+let properlyCopiedObject = {...originObject}; //example how we can avoid duplicating
+properlyCopiedObject.level = 'basic level';
+console.log(originObject);
+console.log(properlyCopiedObject); // properlyCopiedObject can be changed independently from origin
 
 // 4. Demonstrate string's backticks features.
 let title = 'Students';
@@ -64,7 +64,7 @@ let diffTypes = [false, 1, 2, 'three', 'four', 5, {six: 6}];
 
 // 2. Iterate through it and collect amount of each data type to object.
 let collectedTypes = {};
-for(let i = 0; i < diffTypes.length; i++){
+for (let i = 0; i < diffTypes.length; i++){
     let type = typeof diffTypes[i];
     collectedTypes[i] = type;
 };
