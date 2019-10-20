97dc4e26a3cdf95c9380b38cc59fbc67272db887
TamaraNorba <norba.tamara@gmail.com>
2019-10-20 21:14:35 +0300

    Update strings.js

--- Exercises/strings/strings.js
+++ Exercises/strings/strings.js
@@ -1,18 +1,18 @@
 // STRINGS
 // 1. Assign string with unicode to a variable.
-let unicodeStr = 'Example of unicode symbol is \u00B9';
-console.log(unicodeStr);
+let unicodeString = 'Example of unicode symbol is \u00B9';
+console.log(unicodeString);
 
 // 2. Declare a variable with string with escaped symbols.
-let escapeSymb = '\u00A5';
-console.log(escapeSymb);
+let escapeSymbol = '\u00A5';
+console.log(escapeSymbol);
 
 // 3. Declare a variable which will contain name of your favourite animal.
 // 4. Log a message My favourite animal is with dynamic variable from previous task.
-let favorCat = 'Murchyk';
-console.log(`My favourite animal is cat ${favorCat}`);
+let favoriteCat = 'Murchyk';
+console.log(`My favourite animal is cat ${favoriteCat}`);
 
 // 5. Declare a variable which contain name and surname of your favourite writer in lowercase. Log his/her name with capitals first letters.
-let favorWriter = 'isaak asimov';
-let properFavorWriter = favorWriter[0].toUpperCase() + 'saak ' + favorWriter[6].toUpperCase() + 'simov';
-console.log(properFavorWriter);
+let favoriteWriter = 'isaak asimov';
+let properFavoriteWriter = favoriteWriter[0].toUpperCase() + 'saak ' + favoriteWriter[6].toUpperCase() + 'simov';
+console.log(properFavoriteWriter);
