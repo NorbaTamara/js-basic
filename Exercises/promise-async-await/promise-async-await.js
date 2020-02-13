3236ebe1839fdd6219441305f23370dfac2797ce
Tamara Norba <norba.tamara@gmail.com>
2020-02-13 18:47:10 +0200

    homework async/await

--- Exercises/promise-async-await/promise-async-await.js
+++ Exercises/promise-async-await/promise-async-await.js
@@ -1,7 +1,76 @@
+// Make function which will return promise which will resolve
+// in passed to function amount of milliseconds.
+function delay(ms) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => resolve('Resolved promise'), ms);
+  });
+}
+delay(3000).then(result => console.log(result));
+
+// Using fakeAjax do 3 parallel requests.
 const fakeAjax = async () => {
   if (Math.random() > 0.5) {
     return { status: 200 };
   }
-
   throw new Error('Server error!');
 };
+async function parallelRequests() {
+  const result = await Promise.all([
+    fakeAjax(),
+    fakeAjax(),
+    fakeAjax()
+  ]);
+  return result;
+}
+parallelRequests()
+  .then(result => console.log(result))
+  .catch(error => console.log(error.message));
+
+// Catch errors but only for failed executions.
+async function parallelRequests2() {
+  const result = await Promise.all([
+    fakeAjax().catch(error => {
+      return console.log(error.message);
+    }),
+    fakeAjax().catch(error => {
+      return console.log(error.message);
+    }),
+    fakeAjax().catch(error => {
+      return console.log(error.message);
+    })
+  ]);
+  return result;
+}
+parallelRequests2()
+  .then(result => console.log(result));
+
+// Call `fakeAjax` do 3 request in serial
+async function serialRequests() {
+  await fakeAjax().catch(error => {
+    console.log(error.message);
+  });
+  await fakeAjax().catch(error => {
+    console.log(error.message);
+  });
+  await fakeAjax().catch(error => {
+    console.log(error.message);
+  });
+}
+serialRequests();
+
+// Using your `delay` function from first task add delay between each serial request.
+async function delayedSerialRequests() {
+  await fakeAjax().catch(error => {
+    console.log(error.message);
+  });
+  await delay(3000);
+  await fakeAjax().catch(error => {
+    console.log(error.message);
+  });
+  await delay(3000);
+  await fakeAjax().catch(error => {
+    console.log(error.message);
+  });
+  await delay(3000);
+}
+delayedSerialRequests();
